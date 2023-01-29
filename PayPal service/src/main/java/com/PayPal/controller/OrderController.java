0fc5ebab523b803991d371d7d9bb1d8f737df984
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2023-01-29 15:02:41 +0100

    added url for confirming order

--- PayPal service/src/main/java/com/PayPal/controller/OrderController.java
+++ PayPal service/src/main/java/com/PayPal/controller/OrderController.java
@@ -56,7 +56,7 @@ public class OrderController {
 
         HttpEntity<String> request = new HttpEntity<>(obj.toString(), headers);
         CaptureOrderResponseDTO captureOrderResponse = restTemplate.postForObject(pspUrl, request, CaptureOrderResponseDTO.class);
-
+        orderService.browse("http://localhost:4200/confirmation/" + captureOrderResponse.getWebShopOrderId());
         return new ResponseEntity<>(captureOrderResponse, HttpStatus.OK);  // TODO: response je url do web stranice na web shop frontu
     }
 
